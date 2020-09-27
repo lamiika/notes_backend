607e8ecbfb54d73de8efb39dae688492fbe64c55
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-27 13:51:06 +0300

    Added frontend to Heroku build

---
+++ build/precache-manifest.8032e6f3e0e28834ed19f066d1210b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7322e253f45eb34048daaaad884815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499c7cf51c92e966ea29",
+    "url": "/static/css/main.23202dd9.chunk.css"
+  },
+  {
+    "revision": "4bae58adcee35358e065",
+    "url": "/static/js/2.5bf2cba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bf2cba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499c7cf51c92e966ea29",
+    "url": "/static/js/main.45da52af.chunk.js"
+  },
+  {
+    "revision": "fcfa87c657e9ad05204f",
+    "url": "/static/js/runtime-main.ef781745.js"
+  }
+]);
