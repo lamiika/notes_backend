7a6427b329f13d8cb4edc6d83a598e560d4cfdd7
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-02 17:40:46 +0300

    format

--- controllers/notes.js
+++ controllers/notes.js
@@ -36,6 +36,7 @@ notesRouter.post('/', async (request, response, next) => {
   const body = request.body
   const token = getTokenFrom(request)
   const decodedToken = jwt.verify(token, process.env.SECRET)
+
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
